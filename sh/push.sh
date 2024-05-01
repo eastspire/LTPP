fd741a8bac0bf3a1afb53db77ab2e3779e686206
尤雨东 <root@ltpp.vip>
2024-05-01 08:11:15 +0800

    feat:update

--- sh/push.sh
+++ sh/push.sh
@@ -12,7 +12,7 @@ fi
 git commit -m "feat:$update_message";
 git push github main;
 echo -e "\e[32mgithub push finish\e[0m";
-git push ltpp main;
+git push ltpp master;
 echo -e "\e[32mltpp push finish\e[0m";
 echo "Press Enter to continue...";
 read -n 1;
