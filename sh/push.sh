f4400cc4213d11fae8c99fb71128a9d51873380d
尤雨东 <root@ltpp.vip>
2024-12-10 13:11:55 +0800

    feat:mysql

--- sh/push.sh
+++ sh/push.sh
@@ -2,27 +2,32 @@
 git lfs install;
 git config lfs.allowincompletepush true;
 git config --global core.autocrlf input;
-git add .;
-echo -n "Please enter the branch and press Enter:"
-read branch
+echo -n "Please enter the branch and press Enter:";
+read branch;
 if [ -z "$branch" ]; then
-    branch="main"
-fi
-git checkout -b "$branch"
-echo -n "Please enter the update message and press Enter:"
-read update_message
-if [ -z "$update_message" ]; then
-    echo "The update message cannot be empty. Please rerun the script and provide a valid update message."
+    echo "The branch cannot be empty.";
     echo "Press Enter to exit...";
     read -n 1;
-    exit 1
+    exit 1;
+fi
+git checkout -b "$branch";
+git push ltpp "$branch";
+git push origin "$branch";
+git add .;
+echo -n "Please enter the update message and press Enter:";
+read update_message;
+if [ -z "$update_message" ]; then
+    echo "The update message cannot be empty. Please rerun the script and provide a valid update message.";
+    echo "Press Enter to exit...";
+    read -n 1;
+    exit 1;
 fi
 git commit -m "feat:$update_message";
-git push github main;
+git push github "$branch";
 echo -e "\e[32mgithub push finish\e[0m";
-git push ltpp main;
+git push ltpp "$branch";
 echo -e "\e[32mltpp push finish\e[0m";
-git push origin main;
+git push origin "$branch";
 echo -e "\e[32morigin push finish\e[0m";
 git checkout main;
 git merge "$branch";
