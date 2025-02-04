37a3a772f2019ccb4d379a94c2aadd4b9a3eebda
尤雨东 <root@ltpp.vip>
2025-02-04 09:09:28 +0800

    feat: sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/ltpp.git;
+git remote add jihulab ssh://jihulab.com:ltpp/ltpp.git;
 git remote add github git@github.com:ltpp-universe/LTPP.git;
 git remote add origin git@github.com:ltpp-universe/LTPP.git;
